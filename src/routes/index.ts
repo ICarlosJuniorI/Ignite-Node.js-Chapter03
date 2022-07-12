4a578dcb5800e1743bec70e950bc4844f8768b36
Carlos Junior <carlos-junior08@hotmail.com>
2022-07-12 08:39:14 -0300

    Criando controller de usuario

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
+import { usersRoutes } from './users.routes';
 
 const router = Router();
 
 //Com o /categories não precisa ficar passando sempre nas rotas
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,16 +1,23 @@
 import { container } from 'tsyringe';
 
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
+import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository", //Nome do container
   CategoriesRepository    //Classe que vai chamar
-)
+);
 
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 )
