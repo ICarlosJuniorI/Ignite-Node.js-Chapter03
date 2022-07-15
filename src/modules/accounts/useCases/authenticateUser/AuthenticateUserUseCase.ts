9eb87825d2d9e890934ae0171ba5eb70a0fa572a
Carlos Junior <carlos-junior08@hotmail.com>
2022-07-15 08:19:51 -0300

    Criando token do usuário (10:52)

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { inject, injectable } from "tsyringe";
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  },
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    //Verifica se o usuário existe
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    //Verifica se a senha está correta
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    //Gerar jsonwebtoken
+    const token = sign({}, 'f3aae7de49db56ef911a6c5354f6be77', {
+      subject: user.id,
+      expiresIn: '1d'
+    });
+
+    return {
+      user,
+      token
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from 'bcrypt';
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
@@ -13,16 +14,22 @@ class CreateUserUseCase {
 
   async execute({
     name,
-    username,
     password,
     email,
     driver_license
   }: ICreateUserDTO): Promise<void> {
 
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists!');
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      username,
-      password,
+      password: passwordHash,
       email,
       driver_license
     });
