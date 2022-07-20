6f2a40ce7878ad2e230ea55c70b47836a016c645
Carlos Junior <carlos-junior08@hotmail.com>
2022-07-20 07:18:00 -0300

    Tratamento de excecoes

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../errors/AppError";
 import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
@@ -17,7 +19,7 @@ class CreateSpecificationUseCase {
     const specificationAlreadyExists = await this.specificationsRepository.findByName(name);
 
     if (specificationAlreadyExists) {
-      throw new Error('This specification already exists!');
+      throw new AppError('This specification already exists!');
     }
 
     await this.specificationsRepository.create({
